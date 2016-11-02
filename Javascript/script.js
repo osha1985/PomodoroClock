409970b61d54a5ed5810f5a50ec5623c55d6a444
Osher Cohen <osher.y.cohen@gmail.com>
2016-11-02 16:10:01 -0400

    The clock time can now be changed using Session Length controls. Removed jQuery from the project. The project only uses Angular now.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,52 +1,65 @@
-$(document).ready(function() {
-    var degrees = 0;
-    var time = 60;
-    setInterval(function() {
-        $("#slider").css({
-            'transform': 'rotate(' + degrees + 'deg)'
-        });
-        if (degrees === 180) {
-            $("#cover").css("transform", "rotate(180deg)");
-            $("#cover").css("backgroundColor", "white");
+var myApp = angular.module('myApp', []);
+myApp.controller('MainController', function($scope, $interval) {
+    var timer = undefined;
+    $scope.breakLength = 0;
+    $scope.sessionLength = 0;
+    $scope.timerStarted = false;
+    $scope.Math = window.Math;
+    $scope.coverStyle = {};
+    $scope.sliderStyle = {};
+    $scope.decrementBreakLength = function() {
+        if ($scope.breakLength > 0) {
+            $scope.breakLength--;
         }
-        if (degrees > 360) {
-            $("#cover").css("transform", "rotate(0deg)");
-            $("#cover").css("backgroundColor", "blue");
-            $("#slider").css("transform", "rotate(0deg)");
+    };
+    $scope.incrementBreakLength = function() {
+        $scope.breakLength++;
+    };
+    $scope.decrementSessionLength = function() {
+        if ($scope.sessionLength > 0) {
+            $scope.sessionLength--;
+        }
+    };
+    $scope.incrementSessionLength = function() {
+        $scope.sessionLength++;
+    };
+    $scope.toggleTimer = function() {
+        $scope.timerStarted = !$scope.timerStarted;
+        $scope.time = $scope.sessionLength * 60;
+        if (angular.isDefined(timer)) {
+            $interval.cancel(timer);
+            timer = undefined;
+        } else {
+            timer = startTimer($scope, $interval);
+        }
+    }
+});
+
+function startTimer($scope, $interval) {
+    var degrees = 0;
+    var timerInterval = (6 / $scope.sessionLength);
+    var stop = $interval(function() {
+        degrees += timerInterval;
+        $scope.sliderStyle.transform = `rotate(${degrees}deg)`;
+        $scope.time--;
+        if($scope.time <= 0) {
+          $interval.cancel(stop);
+        }
+        if (degrees == 180) {
+            $scope.coverStyle.transform = "rotate(180deg)";
+            $scope.coverStyle.backgroundColor = "white";
+        } else if (degrees >= 360) {
+            $scope.coverStyle.transform = "rotate(0deg)";
+            $scope.coverStyle.backgroundColor = "blue";
+            $scope.coverStyle.transform = "rotate(0deg)";
             degrees = 0;
         }
-        $("#clockText").html(time);
-        degrees += 6;
-        time--;
-        if (time < 0) {
-            time = 60;
-        }
     }, 1000);
-});
-var myApp = angular.module('myApp', []);
-
-myApp.controller('breakLength', function($scope) {
-    $scope.length = 0;
-    $scope.decrement = function() {
-        if($scope.length > 0) {
-          $scope.length--;
-        }
-    };
-
-    $scope.increment = function() {
-        $scope.length++;
-    };
-});
-
-myApp.controller('sessionLength', function($scope) {
-    $scope.length = 0;
-    $scope.decrement = function() {
-      if($scope.length > 0) {
-        $scope.length--;
-      }
-    };
-
-    $scope.increment = function() {
-        $scope.length++;
-    };
-});
+    stop.catch(function() {
+        $scope.sliderStyle.transform = "rotate(0deg)";
+        $scope.coverStyle.transform = "rotate(0deg)";
+        $scope.coverStyle.backgroundColor = "blue";
+        degrees = 0;
+    });
+    return stop;
+}
